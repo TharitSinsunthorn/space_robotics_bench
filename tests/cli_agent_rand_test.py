04ded77113c53e0464533aa9a5ca8483eb797beb
Andrej Orsula <orsula.andrej@gmail.com>
2025-01-29 23:13:54 +0100

    Update test dependencies
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- tests/cli_agent_rand_test.py
+++ tests/cli_agent_rand_test.py
@@ -10,54 +10,59 @@ from srb.utils.cache import read_env_list_cache
 from srb.utils.isaacsim import get_isaacsim_python
 from srb.utils.subprocess import terminate_process
 
-TEST_DURATION: float = 20.0
+DURATION: float = 30.0
+HEADLESS: bool = True
+NUM_ENVS: int = 2
 
 
-if envs := read_env_list_cache():
-
-    @pytest.mark.parametrize("env", envs)
-    def test_cli_agent_rand(env):
-        cmd = (
-            get_isaacsim_python(),
-            "-m",
-            "srb",
-            "agent",
-            "rand",
-            "--headless",
-            "--env",
-            env,
-        )
-
-        environ = os.environ.copy()
-        environ["SF_BAKER"] = "false"
-
-        try:
-            process = subprocess.Popen(
-                cmd,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                preexec_fn=os.setsid,
-                text=True,
-                env=environ,
-            )
-
-            start = time.time()
-            while time.time() - start < TEST_DURATION:
-                if process.poll():
-                    stdout, stderr = process.communicate()
-                    pytest.fail(
-                        f'Process failed for env "{env}"\n[env={env}] STDOUT:\n{stdout}\n[env={env}] STDERR:\n{stderr}'
-                    )
-                time.sleep(0.1)
-        except Exception as e:
-            pytest.fail(
-                f'Failed to start process for env "{env}"\n[env={env}] Exception: {e}'
-            )
-        finally:
-            terminate_process(cmd, process)
-
-else:
+envs = read_env_list_cache()
+if not envs:
     test_filepath = Path(__file__)
     logging.warning(
-        f"Skipping {test_filepath.parent.name}/{test_filepath.name} because no environments were found in the cache"
+        f"Skipping {test_filepath.parent.name}/{test_filepath.name} because no environments were found in the cache (repeat the test)"
     )
+
+
+@pytest.mark.order(after="cli_ls_test.py::test_cli_ls")
+@pytest.mark.parametrize("env", envs or [])
+def test_cli_agent_rand(env):
+    cmd = (
+        get_isaacsim_python(),
+        "-m",
+        "srb",
+        "agent",
+        "rand",
+        "--headless" if HEADLESS else "--hide_ui",
+        "--env",
+        env,
+        f"env.scene.num_envs={NUM_ENVS}",
+    )
+
+    environ = os.environ.copy()
+    environ["SF_BAKER"] = "0"
+
+    process = None
+    try:
+        process = subprocess.Popen(
+            cmd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            preexec_fn=os.setsid,
+            text=True,
+            env=environ,
+        )
+
+        start = time.time()
+        while time.time() - start < DURATION:
+            if process.poll() is not None:
+                stdout, stderr = process.communicate()
+                pytest.fail(
+                    f'Process failed for env "{env}"\n[env={env}] STDOUT:\n{stdout}\n[env={env}] STDERR:\n{stderr}'
+                )
+            time.sleep(0.1)
+    except Exception as e:
+        pytest.fail(
+            f'Failed to start process for env "{env}"\n[env={env}] Exception: {e}'
+        )
+    finally:
+        terminate_process("srb", process)
--- tests/import_test.py
+++ tests/import_test.py
@@ -1,2 +1,6 @@
+import pytest
+
+
+@pytest.mark.order(1)
 def test_import():
     import srb  # noqa: F401
