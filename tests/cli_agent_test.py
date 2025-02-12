0db8dc803b694a8b90288068d1b5694e9f9d585c
Andrej Orsula <orsula.andrej@gmail.com>
2025-02-12 14:47:18 +0100

    Add test for training agents on all evns
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- tests/cli_agent_test.py
+++ tests/cli_agent_test.py
@@ -1,6 +1,7 @@
 import math
 import os
 import subprocess
+import tempfile
 import time
 from pathlib import Path
 from typing import Iterable
@@ -16,7 +17,8 @@ HEADLESS: bool = True
 TEST_VISUAL_ENVS: bool = True
 
 MAX_NUM_ENVS: int = 256
-MAX_DURATION: float = 20.0
+MAX_DURATION_RAND: float = 20.0
+MAX_DURATION_TRAIN: float = 60.0
 
 
 def _list_envs() -> Iterable[str]:
@@ -79,14 +81,14 @@ def test_cli_agent_rand(env: str, num_envs: int):
         )
 
         start = time.time()
-        while time.time() - start < MAX_DURATION:
+        while time.time() - start < MAX_DURATION_RAND:
             if process.poll() is not None:
                 logging.critical(f"[{env}] Failed command: {' '.join(cmd)}")
                 stdout, stderr = process.communicate()
                 pytest.fail(
                     f'Process failed for env "{env}"\n[env={env}] STDOUT:\n{stdout}\n[env={env}] STDERR:\n{stderr}'
                 )
-            time.sleep(0.01 * MAX_DURATION)
+            time.sleep(0.01 * MAX_DURATION_RAND)
     except Exception as e:
         logging.critical(f"[{env}] Failed command: {' '.join(cmd)}")
         pytest.fail(
@@ -94,3 +96,70 @@ def test_cli_agent_rand(env: str, num_envs: int):
         )
     finally:
         terminate_process("srb", process)
+
+
+@pytest.mark.parametrize(
+    "env,num_envs,algo",
+    (
+        (env, num_envs, algo)
+        for env in _list_envs()
+        for num_envs in (
+            (1, math.floor(MAX_NUM_ENVS ** (1.0 / 3.0))) if MAX_NUM_ENVS > 1 else (1,)
+        )
+        for algo in ("dreamer",)
+        if (not _is_visual_env(env) or (num_envs < MAX_NUM_ENVS and MAX_NUM_ENVS > 1))
+    ),
+)
+def test_cli_agent_train(env: str, num_envs: int, algo: str):
+    with tempfile.TemporaryDirectory(prefix="srb_") as tmpdir:
+        match algo:
+            case _:
+                agent_params = ()
+
+        cmd = (
+            get_isaacsim_python(),
+            "-m",
+            "srb",
+            "agent",
+            "train",
+            "--headless" if HEADLESS else "--hide_ui",
+            f"env.scene.num_envs={num_envs}",
+            "--env",
+            env,
+            "--algo",
+            algo,
+            "--logs",
+            tmpdir,
+            *agent_params,
+        )
+
+        environ = os.environ.copy()
+        environ["SF_BAKER"] = "0"
+
+        process = None
+        try:
+            process = subprocess.Popen(
+                cmd,
+                env=environ,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True,
+                preexec_fn=os.setsid,
+            )
+
+            start = time.time()
+            while time.time() - start < MAX_DURATION_TRAIN:
+                if process.poll() is not None:
+                    logging.critical(f"[{env}] Failed command: {' '.join(cmd)}")
+                    stdout, stderr = process.communicate()
+                    pytest.fail(
+                        f'Process failed for env "{env}"\n[env={env}] STDOUT:\n{stdout}\n[env={env}] STDERR:\n{stderr}'
+                    )
+                time.sleep(0.01 * MAX_DURATION_TRAIN)
+        except Exception as e:
+            logging.critical(f"[{env}] Failed command: {' '.join(cmd)}")
+            pytest.fail(
+                f'Failed to start process for env "{env}"\n[env={env}] Exception: {e}'
+            )
+        finally:
+            terminate_process("srb", process)
