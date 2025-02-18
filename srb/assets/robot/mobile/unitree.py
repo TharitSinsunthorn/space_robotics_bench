13031abdff68fdb634ba5a3556830650b72d3a48
Tharit Sinsunthorn <tharit.sinsunthorn.r1@dc.tohoku.ac.jp>
2025-02-18 18:46:03 +0900

    add unitree go2 to the asset

--- srb/assets/robot/mobile/unitree.py
+++ srb/assets/robot/mobile/unitree.py
@@ -1,7 +1,7 @@
 from isaaclab.sim import UsdFileCfg
 
 from srb.core.action import JointPosition, JointPositionActionCfg
-from srb.core.actuator import ActuatorNetMLPCfg
+from srb.core.actuator import ActuatorNetMLPCfg, DCMotorCfg
 from srb.core.asset import ArticulationCfg, Frame, LeggedRobot
 from srb.core.sim import ArticulationRootPropertiesCfg, RigidBodyPropertiesCfg
 from srb.utils.nucleus import ISAACLAB_NUCLEUS_DIR, get_local_or_nucleus_path
@@ -73,6 +73,71 @@ class UnitreeGo1(LeggedRobot):
     ## Actions
     action_cfg: JointPosition = JointPosition(
         pos=JointPositionActionCfg(asset_name="robot", joint_names=[".*"], scale=0.5)
+    )
+
+    ## Frames
+    frame_base: Frame = Frame(prim_relpath="base")
+
+    ## Links
+    regex_links_feet: str = ".*foot"
+    regex_links_undesired_contacts: str = "(trunk|.*hip|.*thigh)"
+    
+
+class UnitreeGo2(LeggedRobot):
+    ## Model
+    asset_cfg: ArticulationCfg = ArticulationCfg(
+        prim_path="{ENV_REGEX_NS}/robot",
+        spawn=UsdFileCfg(
+            usd_path=get_local_or_nucleus_path(
+                SRB_ASSETS_DIR_SRB_ROBOT.joinpath("Unitree")
+                .joinpath("Go2")
+                .joinpath("go2.usd"),
+                f"{ISAACLAB_NUCLEUS_DIR}/Robots/Unitree/Go2/go2.usd",
+            ),
+            activate_contact_sensors=True,
+            rigid_props=RigidBodyPropertiesCfg(
+                disable_gravity=False,
+                retain_accelerations=False,
+                linear_damping=0.0,
+                angular_damping=0.0,
+                max_linear_velocity=1000.0,
+                max_angular_velocity=1000.0,
+                max_depenetration_velocity=1.0,
+            ),
+            articulation_props=ArticulationRootPropertiesCfg(
+                enabled_self_collisions=False,
+                solver_position_iteration_count=4,
+                solver_velocity_iteration_count=0,
+            ),
+        ),
+        init_state=ArticulationCfg.InitialStateCfg(
+            pos=(0.0, 0.0, 0.4),
+            joint_pos={
+                ".*L_hip_joint": 0.1,
+                ".*R_hip_joint": -0.1,
+                "F[L,R]_thigh_joint": 0.8,
+                "R[L,R]_thigh_joint": 1.0,
+                ".*_calf_joint": -1.5,
+            },
+            joint_vel={".*": 0.0},
+        ),
+        soft_joint_pos_limit_factor=0.9,
+        actuators={
+        "base_legs": DCMotorCfg(
+            joint_names_expr=[".*_hip_joint", ".*_thigh_joint", ".*_calf_joint"],
+            effort_limit=23.5,
+            saturation_effort=23.5,
+            velocity_limit=30.0,
+            stiffness=25.0,
+            damping=0.5,
+            friction=0.0,
+        ),
+    },
+    )
+
+    ## Actions
+    action_cfg: JointPosition = JointPosition(
+        pos=JointPositionActionCfg(asset_name="robot", joint_names=[".*"], scale=0.25)
     )
 
     ## Frames
