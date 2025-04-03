a8ceaff014ec5ec434c08059c0ab6a5677139c35
Andrej Orsula <orsula.andrej@gmail.com>
2025-04-03 22:53:15 +0200

    Add Robotiq Hand-E
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- srb/assets/object/tool/__init__.py
+++ srb/assets/object/tool/__init__.py
@@ -1,5 +1,7 @@
 from .allegro_hand import AllegroHand  # noqa: F401
 from .franka_hand import FrankaHand  # noqa: F401
 from .kinova_gripper import Kinova300  # noqa: F401
+from .robotiq import RobotiqHandE  # noqa: F401
 from .scoop import Scoop  # noqa: F401
 from .shadow_hand import ShadowHand  # noqa: F401
+from .so_arm100 import SOArm100Gripper  # noqa: F401
