d32ae89290daabb4ac6f855f2f1a0edbd3f1b404
Andrej Orsula <orsula.andrej@gmail.com>
2025-04-18 00:15:23 +0200

    Add LunaLab
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- srb/assets/scenery/__init__.py
+++ srb/assets/scenery/__init__.py
@@ -1,3 +1,4 @@
 from .ground_plane import GroundPlane  # noqa: F401
+from .lab import LunaLab  # noqa: F401
 from .planetary_surface import MarsSurface, MoonSurface  # noqa: F401
 from .spacecraft import StaticGateway, StaticVenusExpress  # noqa: F401
---
+++ srb/assets/scenery/lab.py
@@ -0,0 +1,34 @@
+from srb.core.asset import AssetBaseCfg, Subterrane
+from srb.core.sim import CollisionPropertiesCfg, GridParticlesSpawnerCfg, UsdFileCfg
+from srb.utils.path import SRB_ASSETS_DIR_SRB_SCENERY
+
+PARTICLE_SIZE = 0.025
+
+
+class LunaLab(Subterrane):
+    asset_cfg: AssetBaseCfg = AssetBaseCfg(
+        prim_path="{ENV_REGEX_NS}/lunalab",
+        spawn=UsdFileCfg(
+            usd_path=SRB_ASSETS_DIR_SRB_SCENERY.joinpath("lunalab.usdc").as_posix(),
+            collision_props=CollisionPropertiesCfg(),
+        ),
+        init_state=AssetBaseCfg.InitialStateCfg(pos=(-4.25, -5.5, 0.0)),
+    )
+
+    _regolith: AssetBaseCfg = AssetBaseCfg(
+        prim_path="{ENV_REGEX_NS}/regolith",
+        spawn=GridParticlesSpawnerCfg(
+            ratio=0.4,
+            particle_size=PARTICLE_SIZE,
+            dim_x=round(6.5 / PARTICLE_SIZE),
+            dim_y=round(11.0 / PARTICLE_SIZE),
+            dim_z=round(0.5 / PARTICLE_SIZE),
+            velocity=((-0.2, 0.2), (-0.2, 0.2), (-0.2, 0.0)),
+            fluid=False,
+            friction=0.4,
+            cohesion=0.02,
+            density=300.0,
+            cast_shadows=False,
+        ),
+        init_state=AssetBaseCfg.InitialStateCfg(pos=(0.0, 0.0, -0.3)),
+    )
