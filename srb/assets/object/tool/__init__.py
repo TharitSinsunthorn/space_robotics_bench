d394fb730049431b366e0a847e4449ed77a32171
Andrej Orsula <orsula.andrej@gmail.com>
2025-04-20 23:46:35 +0200

    Add screwdriver tools
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- srb/assets/object/tool/__init__.py
+++ srb/assets/object/tool/__init__.py
@@ -3,5 +3,10 @@ from .franka_hand import FrankaHand  # noqa: F401
 from .kinova_gripper import Kinova300  # noqa: F401
 from .robotiq import RobotiqHandE  # noqa: F401
 from .scoop import Scoop  # noqa: F401
+from .screwdriver import (  # noqa: F401
+    ElectricScrewdriverM3,
+    ManualScrewdriverM3,
+    ManualScrewdriverM5,
+)
 from .shadow_hand import ShadowHand  # noqa: F401
 from .so_arm100 import SOArm100Gripper  # noqa: F401
---
+++ srb/assets/object/tool/screwdriver.py
@@ -0,0 +1,110 @@
+from math import pi
+
+from srb.core.action import (
+    ActionGroup,
+    JointVelocityActionCfg,
+    JointVelocityActionGroup,
+)
+from srb.core.actuator import ImplicitActuatorCfg
+from srb.core.asset import (
+    ActiveTool,
+    ArticulationCfg,
+    Frame,
+    RigidObjectCfg,
+    Tool,
+    Transform,
+)
+from srb.core.sim import (
+    ArticulationRootPropertiesCfg,
+    CollisionPropertiesCfg,
+    MassPropertiesCfg,
+    MeshCollisionPropertiesCfg,
+    RigidBodyPropertiesCfg,
+    UsdFileCfg,
+)
+from srb.utils.path import SRB_ASSETS_DIR_SRB_OBJECT, SRB_ASSETS_DIR_SRB_ROBOT
+
+
+class ManualScrewdriverM3(Tool):
+    asset_cfg: RigidObjectCfg = RigidObjectCfg(
+        prim_path="{ENV_REGEX_NS}/screwdriver",
+        spawn=UsdFileCfg(
+            usd_path=(
+                SRB_ASSETS_DIR_SRB_OBJECT.joinpath("screwdriver_m3.usdz").as_posix()
+            ),
+            activate_contact_sensors=True,
+            collision_props=CollisionPropertiesCfg(),
+            mesh_collision_props=MeshCollisionPropertiesCfg(mesh_approximation="sdf"),
+            rigid_props=RigidBodyPropertiesCfg(),
+            mass_props=MassPropertiesCfg(density=1500.0),
+        ),
+    )
+
+    ## Frames
+    frame_mount: Frame = Frame(prim_relpath="screwdriver")
+    frame_tool_centre_point: Frame = Frame(offset=Transform(pos=(0.0, 0.0, 0.1)))
+
+
+class ManualScrewdriverM5(Tool):
+    asset_cfg: RigidObjectCfg = RigidObjectCfg(
+        prim_path="{ENV_REGEX_NS}/screwdriver",
+        spawn=UsdFileCfg(
+            usd_path=(
+                SRB_ASSETS_DIR_SRB_OBJECT.joinpath("screwdriver_m5.usdz").as_posix()
+            ),
+            activate_contact_sensors=True,
+            collision_props=CollisionPropertiesCfg(),
+            mesh_collision_props=MeshCollisionPropertiesCfg(mesh_approximation="sdf"),
+            rigid_props=RigidBodyPropertiesCfg(),
+            mass_props=MassPropertiesCfg(density=1500.0),
+        ),
+    )
+
+    ## Frames
+    frame_mount: Frame = Frame(prim_relpath="screwdriver")
+    frame_tool_centre_point: Frame = Frame(offset=Transform(pos=(0.0, 0.0, 0.1)))
+
+
+class ElectricScrewdriverM3(ActiveTool):
+    ## Model
+    asset_cfg: ArticulationCfg = ArticulationCfg(
+        prim_path="{ENV_REGEX_NS}/electric_screwdriver",
+        spawn=UsdFileCfg(
+            usd_path=SRB_ASSETS_DIR_SRB_ROBOT.joinpath("tool")
+            .joinpath("electric_screwdriver_m3.usdz")
+            .as_posix(),
+            activate_contact_sensors=True,
+            collision_props=CollisionPropertiesCfg(
+                contact_offset=0.005, rest_offset=0.0
+            ),
+            rigid_props=RigidBodyPropertiesCfg(
+                disable_gravity=True,
+                max_depenetration_velocity=5.0,
+            ),
+            articulation_props=ArticulationRootPropertiesCfg(
+                enabled_self_collisions=False,
+                solver_position_iteration_count=8,
+                solver_velocity_iteration_count=0,
+            ),
+        ),
+        actuators={
+            "driver": ImplicitActuatorCfg(
+                joint_names_expr=["driver_joint"],
+                velocity_limit=4.0 * pi,
+                effort_limit=100.0,
+                stiffness=0.0,
+                damping=5000.0,
+            ),
+        },
+    )
+
+    ## Actions
+    actions: ActionGroup = JointVelocityActionGroup(
+        JointVelocityActionCfg(
+            asset_name="robot", joint_names=["driver_joint"], scale=2.0
+        ),
+    )
+
+    ## Frames
+    frame_mount: Frame = Frame(prim_relpath="base")
+    frame_tool_centre_point: Frame = Frame(offset=Transform(pos=(0.0, 0.0, 0.0776)))
