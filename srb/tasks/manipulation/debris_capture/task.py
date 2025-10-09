723e1f6229006c31358f6a6ddf4f9a87e478e8d3
Andrej Orsula <orsula.andrej@gmail.com>
2025-10-08 17:45:00 -0700

    Update tasks
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- srb/tasks/manipulation/debris_capture/task.py
+++ srb/tasks/manipulation/debris_capture/task.py
@@ -9,6 +9,7 @@ from srb.core.asset import (
     Articulation,
     AssetVariant,
     ExtravehicularScenery,
+    MobileRobot,
     Object,
     RigidObject,
     RigidObjectCfg,
@@ -28,6 +29,7 @@ from srb.utils.math import (
     deg_to_rad,
     matrix_from_quat,
     rotmat_to_rot6d,
+    rpy_to_quat,
     scale_transform,
     subtract_frame_transforms,
 )
@@ -77,7 +79,12 @@ class TaskCfg(ManipulationEnvCfg):
     domain: Domain = Domain.ORBIT
 
     ## Assets
-    scenery: ExtravehicularScenery | AssetVariant | None = assets.StaticVenusExpress()
+    scenery: ExtravehicularScenery | MobileRobot | AssetVariant | None = (
+        assets.VenusExpress()
+    )
+    scenery.asset_cfg.init_state.pos = (-0.55, 0.0, -0.35)
+    scenery.asset_cfg.init_state.rot = rpy_to_quat(0.0, 0.0, 90.0)
+
     _scenery: ExtravehicularScenery = MISSING  # type: ignore
     pedestal: Object | AssetVariant | None = None
     debris: Object | AssetVariant | None = AssetVariant.DATASET
@@ -282,8 +289,8 @@ def _compute_step_return(
     ## Rewards ##
     #############
     # Penalty: Action rate
-    WEIGHT_ACTION_RATE = -0.05
-    penalty_action_rate = WEIGHT_ACTION_RATE * torch.sum(
+    WEIGHT_ACTION_RATE = -0.5
+    penalty_action_rate = WEIGHT_ACTION_RATE * torch.mean(
         torch.square(act_current - act_previous), dim=1
     )
 
